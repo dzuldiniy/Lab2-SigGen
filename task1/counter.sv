a449765c97297c17a52c8f0d077d21b9b88f2e62
Dzuldiniy Hussain <dzuldiniy.h@gmail.com>
2025-10-25 02:35:47 +0100

    completed task1

---
+++ task1/counter.sv
@@ -0,0 +1,15 @@
+module counter #(
+  parameter WIDTH = 8
+)(
+  // interface signals
+  input  logic             clk,      // clock
+  input  logic             rst,      // reset
+  input  logic             en,       // load counter from data
+  input  logic [WIDTH-1:0] incr,        // value to preload
+  output logic [WIDTH-1:0] count     // count output
+);
+
+always_ff @ (posedge clk)
+  if (rst) count <= {WIDTH{1'b0}};
+  else     count <= count + (incr * en);
+endmodule
